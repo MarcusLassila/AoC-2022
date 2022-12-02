5926a63c4ecaaa10a5d6592ec8a7cfbb25375c3b
Marcus Lassila <marcus@ducce.net>
2022-12-02 08:09:40 +0100

    day2

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -1,23 +1,42 @@
-#include <bits/stdc++.h>
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <utility>
+#include <vector>
 
 static const std::array<std::string, 2> input_files {"sample.txt", "input.txt"};
 
+int part_one(const std::vector<std::pair<char, char>>& rounds) {
+    return std::accumulate(rounds.cbegin(), rounds.cend(), 0, [](int score, const auto& round) -> int {
+        const auto& [p, q] = round;
+        return score + q - 'X' + 1 + (4 - (p - 'A' - q + 'X') % 3) % 3 * 3;
+    });
+}
+
+int part_two(const std::vector<std::pair<char, char>>& rounds) {
+    return std::accumulate(rounds.cbegin(), rounds.cend(), 0, [](int score, const auto& round) -> int {
+        const auto& [p, q] = round;
+        return score + 3 * (q - 'X') + (p - 'A' + q - 'Y' + 3) % 3 + 1;
+    });
+}
+
 int main() {
-    std::ifstream input;
     for (const auto& file : input_files) {
-        input.open(file);
+        std::ifstream input {file};
         if (!input) {
             std::cerr << "Failed to open: " << file << '\n';
             return 1;
         }
         std::string line;
+        std::vector<std::pair<char, char>> rounds;
         while (std::getline(input, line)) {
-            std::cout << line << '\n';
+            rounds.emplace_back(std::make_pair(line[0], line[2]));
         }
         std::cout << file << ":\n";
-        std::cout << "Answer part 1:  " << "..." << '\n';
-        std::cout << "Answer part 2:  " << "..." << '\n';
-        input.close();
+        std::cout << "Answer part 1:  " << part_one(rounds) << '\n';
+        std::cout << "Answer part 2:  " << part_two(rounds) << '\n';
     }
     return 0;
 }
