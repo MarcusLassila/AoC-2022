af44e4ccd59d9c67dc260e53dd810eef88c54299
Marcus Lassila <marcus@ducce.net>
2022-12-02 08:47:21 +0100

    day2 simplify

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -1,26 +1,9 @@
 #include <array>
 #include <fstream>
 #include <iostream>
-#include <numeric>
 #include <string>
-#include <utility>
-#include <vector>
 
 static const std::array<std::string, 2> input_files {"sample.txt", "input.txt"};
-
-int part_one(const std::vector<std::pair<char, char>>& rounds) {
-    return std::accumulate(rounds.cbegin(), rounds.cend(), 0, [](int score, const auto& round) -> int {
-        const auto& [p, q] = round;
-        return score + q - 'X' + 1 + (4 - (p - 'A' - q + 'X') % 3) % 3 * 3;
-    });
-}
-
-int part_two(const std::vector<std::pair<char, char>>& rounds) {
-    return std::accumulate(rounds.cbegin(), rounds.cend(), 0, [](int score, const auto& round) -> int {
-        const auto& [p, q] = round;
-        return score + 3 * (q - 'X') + (p - 'A' + q - 'Y' + 3) % 3 + 1;
-    });
-}
 
 int main() {
     for (const auto& file : input_files) {
@@ -29,14 +12,18 @@ int main() {
             std::cerr << "Failed to open: " << file << '\n';
             return 1;
         }
+        int score_one = 0;
+        int score_two = 0;
         std::string line;
-        std::vector<std::pair<char, char>> rounds;
         while (std::getline(input, line)) {
-            rounds.emplace_back(std::make_pair(line[0], line[2]));
+            char p = line[0];
+            char q = line[2];
+            score_one += q - 'X' + 1 + (4 - (p - 'A' - q + 'X') % 3) % 3 * 3;
+            score_two += 3 * (q - 'X') + (p - 'A' + q - 'Y' + 3) % 3 + 1;
         }
         std::cout << file << ":\n";
-        std::cout << "Answer part 1:  " << part_one(rounds) << '\n';
-        std::cout << "Answer part 2:  " << part_two(rounds) << '\n';
+        std::cout << "Answer part 1:  " << score_one << '\n';
+        std::cout << "Answer part 2:  " << score_two << '\n';
     }
     return 0;
 }
