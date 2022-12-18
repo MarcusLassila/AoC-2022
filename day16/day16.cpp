36fca3bd8ee2b02ec86139249f6558fc90ad6608
Marcus Lassila <marcus@ducce.net>
2022-12-19 00:26:23 +0100

    day16 some refactoring and speed up

--- day16/day16.cpp
+++ day16/day16.cpp
@@ -1,19 +1,20 @@
-#include <bits/stdc++.h>
-
-static constexpr int time_limit = 30U;
-
-struct Int {
-    int value;
-    Int() : value(-1) {}
-    Int(int value) : value(value) {}
-
-    operator int() const { return value; }
-};
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+//#include <unordered_map>  // Maybe remove?
+//#include <unordered_set>  // Maybe remove?
+#include <vector>
 
 struct Valve {
     int flow;
     std::string name;
-    Valve() : flow(0) {}
+    Valve() : Valve{"", 0} {}
     Valve(const std::string& name, int flow) : flow(flow), name(name) {}
 
     friend std::ostream& operator<<(std::ostream& os, const Valve& valve);
@@ -38,11 +39,20 @@ bool operator<(const Valve& a, const Valve& b) {
     return a.flow < b.flow || (a.flow == b.flow && a.name < b.name);
 }
 
+template<>
+struct std::hash<Valve> {
+    std::size_t operator()(Valve const& valve) const noexcept {
+        std::size_t h1 = std::hash<int>{}(valve.flow);
+        std::size_t h2 = std::hash<std::string>{}(valve.name);
+        return h1 ^ (h2 << 1);
+    }
+};
+
 struct TValve {
     Valve valve;
     int time;
     TValve(const Valve& valve, int time) : valve(valve), time(time) {}
-    TValve(const std::string& name, int flow, int time) : valve(Valve{name, flow}), time(time) {}
+    TValve(const std::string& name, int flow, int time) : TValve{Valve{name, flow}, time} {}
 };
 
 using WGraph = std::map<Valve, std::vector<TValve>>;
@@ -64,15 +74,20 @@ struct Vertex {
     int time;
     int weight;
     std::set<std::string> activated;
-    Vertex(const Valve& valve, int time)
+    Vertex(const Valve& valve, int time, const std::set<std::string>& activated)
         : valve(valve)
         , time(time)
         , weight(time * valve.flow)
+        , activated(activated)
+        {}
+    Vertex(const Valve& valve, int time)
+        : Vertex{valve, time, {}}
         {}
     Vertex(const std::string& name, int flow, int time)
-        : valve(Valve{name, flow})
-        , time(time)
-        , weight(time * flow)
+        : Vertex{Valve{name, flow}, time, {}}
+        {}
+    Vertex(const std::string& name, int flow, int time, const std::set<std::string>& activated)
+        : Vertex{Valve{name, flow}, time, activated}
         {}
 
     friend std::ostream& operator<<(std::ostream& os, const Vertex& vertex);
@@ -89,40 +104,6 @@ std::ostream& operator<<(std::ostream& os, const Vertex& vertex) {
 
 bool operator<(const Vertex& a, const Vertex& b) {
     return a.weight < b.weight;  // If don't work, compare time and valves as well.
-}
-
-int dijkstra(const WGraph& graph) {
-    std::map<TValve, int> weight;
-    Vertex start {"AA", 0, 30};
-    std::priority_queue<Vertex> pq;
-    pq.push(start);
-    while (!pq.empty()) {
-        Vertex curr = pq.top();
-        //std::cout << "Pop vertex: " << curr << '\n';
-        pq.pop();
-        if (curr.time <= 0) {
-            continue;
-        }
-        for (const auto& adj : graph.at(curr.valve)) {
-            if (curr.time > adj.time && adj.valve.flow > 0 && !curr.activated.count(adj.valve.name)) {
-                Vertex next {adj.valve, curr.time - adj.time - 1};
-                int w = next.weight + curr.weight;
-                TValve node {adj.valve, next.time};
-                if (w > weight[node]) {
-                    next.activated = curr.activated;
-                    next.activated.insert(adj.valve.name);
-                    next.weight = w;
-                    weight[node] = w;
-                    pq.push(next);
-                }
-            }
-        }
-    }
-    int max_weight = 0;
-    for (const auto& [n, w] : weight) {
-        max_weight = std::max(max_weight, w);
-    }
-    return max_weight;
 }
 
 struct DTValve {
@@ -155,91 +136,120 @@ bool operator<(const DVertex& a, const DVertex& b) {
     return a.weight() < b.weight();
 }
 
-int double_dijkstra(const WGraph& graph) {
+
+int single_dijkstra(const WGraph& graph, const Valve& start_at, int time_limit) {
+    std::map<TValve, int> weight;
+    Vertex start {start_at, time_limit};
+    std::priority_queue<Vertex> pq;
+    pq.push(start);
+    while (!pq.empty()) {
+        Vertex curr = pq.top();
+        pq.pop();
+        if (curr.time <= 0) {
+            continue;
+        }
+        for (const auto& adj : graph.at(curr.valve)) {
+            if (curr.time <= adj.time || curr.activated.count(adj.valve.name)) {
+                continue;
+            }
+            Vertex next {adj.valve, curr.time - adj.time - 1};
+            int w = next.weight + curr.weight;
+            TValve node {adj.valve, next.time};
+            if (w > weight[node]) {
+                next.activated = curr.activated;
+                next.activated.insert(adj.valve.name);
+                next.weight  = w;
+                weight[node] = w;
+                pq.push(next);
+            }
+        }
+    }
+
+    return std::accumulate(weight.cbegin(), weight.cend(), 0, [](int acc, const auto& w) -> int {
+        return std::max(acc, w.second);
+    });
+}
+
+int double_dijkstra(const WGraph& graph, const Valve& start_at, int time_limit) {
     std::map<DTValve, int> weight;
-    Vertex start {"AA", 0, 26};
+    Vertex start {start_at, time_limit};
     std::priority_queue<DVertex> pq;
     pq.emplace(DVertex{start, start});
 
-    const auto can_move = [&](const DVertex& dv) -> std::pair<bool, bool> {
-        std::vector<TValve> adjs = graph.at(dv.v1.valve);
-        bool a = std::any_of(adjs.begin(), adjs.end(), [&](const TValve& tv) -> bool {
-            return dv.v1.time > tv.time && !dv.isActivated(tv.valve.name);
-        });
-        adjs = graph.at(dv.v2.valve);
-        bool b = std::any_of(adjs.begin(), adjs.end(), [&](const TValve& tv) -> bool {
-            return dv.v2.time > tv.time && !dv.isActivated(tv.valve.name);
-        });
-        return {a, b};
-    };
-
-    const auto double_activation = [](const DVertex& dv) -> bool {
-        for (const auto& n : dv.v1.activated) {
-            if (dv.v2.activated.count(n)) {
-                return true;
-            }
-        }
-        return false;
-    };
+    // const auto double_activation = [](const DVertex& dv) -> bool {
+    //     for (const auto& n : dv.v1.activated) {
+    //         if (dv.v2.activated.count(n)) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // };
 
     while (!pq.empty()) {
         DVertex curr = pq.top();
-        
         pq.pop();
-        const auto& [can_move_1, can_move_2] = can_move(curr);
+
+        std::vector<TValve> filtered_adjs_1;
+        std::vector<TValve> adjs_1 = graph.at(curr.v1.valve);
+        std::copy_if(adjs_1.begin(), adjs_1.end(), std::back_inserter(filtered_adjs_1), [&curr](const auto& adj) -> bool {
+            return curr.v1.time > adj.time && !curr.isActivated(adj.valve.name);
+        });
+        std::vector<TValve> filtered_adjs_2;
+        std::vector<TValve> adjs_2 = graph.at(curr.v2.valve);
+        std::copy_if(adjs_2.begin(), adjs_2.end(), std::back_inserter(filtered_adjs_2), [&curr](const auto& adj) -> bool {
+            return curr.v2.time > adj.time && !curr.isActivated(adj.valve.name);
+        });
+        bool can_move_1 = !filtered_adjs_1.empty();
+        bool can_move_2 = !filtered_adjs_2.empty();
+
         if (!can_move_1 && !can_move_2) {
             continue;
         } else if (!can_move_1) {
-            for (const auto& adj : graph.at(curr.v2.valve)) {
-                if (curr.v2.time > adj.time && adj.valve.flow > 0 && !curr.isActivated(adj.valve.name)) {
-                    Vertex next {adj.valve, curr.v2.time - adj.time - 1};
-                    int w = next.weight + curr.weight();
-                    DTValve node {TValve{curr.v1.valve, curr.v1.time}, TValve{adj.valve, next.time}};
-                    if (w > weight[node]) {
-                        next.activated = curr.v2.activated;
-                        next.activated.insert(adj.valve.name);
-                        next.weight = curr.v2.weight + next.weight;
-                        weight[node] = w;
-                        pq.push(DVertex{curr.v1, next});
-                    }
+            for (const auto& adj : filtered_adjs_2) {
+                Vertex next {adj.valve, curr.v2.time - adj.time - 1};
+                int w = next.weight + curr.weight();
+                DTValve node {TValve{curr.v1.valve, curr.v1.time}, TValve{adj.valve, next.time}};
+                if (w > weight[node]) {
+                    next.activated = curr.v2.activated;
+                    next.activated.insert(adj.valve.name);
+                    next.weight  = curr.v2.weight + next.weight;
+                    weight[node] = w;
+                    pq.push(DVertex{curr.v1, next});
                 }
             }
         } else if (!can_move_2) {
-            for (const auto& adj : graph.at(curr.v1.valve)) {
-                if (curr.v1.time > adj.time && adj.valve.flow > 0 && !curr.isActivated(adj.valve.name)) {
-                    Vertex next {adj.valve, curr.v1.time - adj.time - 1};
-                    int w = next.weight + curr.weight();
-                    DTValve node {TValve{adj.valve, next.time}, TValve{curr.v2.valve, curr.v2.time}};
-                    if (w > weight[node]) {
-                        next.activated = curr.v1.activated;
-                        next.activated.insert(adj.valve.name);
-                        next.weight = curr.v1.weight + next.weight;
-                        weight[node] = w;
-                        pq.push(DVertex{next, curr.v2});
-                    }
+            for (const auto& adj : filtered_adjs_1) {
+                Vertex next {adj.valve, curr.v1.time - adj.time - 1};
+                int w = next.weight + curr.weight();
+                DTValve node {TValve{adj.valve, next.time}, TValve{curr.v2.valve, curr.v2.time}};
+                if (w > weight[node]) {
+                    next.activated = curr.v1.activated;
+                    next.activated.insert(adj.valve.name);
+                    next.weight  = curr.v1.weight + next.weight;
+                    weight[node] = w;
+                    pq.push(DVertex{next, curr.v2});
                 }
             }
         } else {
-            for (const auto& adj_1 : graph.at(curr.v1.valve)) {
-                if (curr.v1.time <= adj_1.time || curr.isActivated(adj_1.valve.name)) {
-                    continue;
-                }
-                Vertex next_1 {adj_1.valve, curr.v1.time - adj_1.time - 1};
-                next_1.activated = curr.v1.activated;
-                next_1.activated.insert(adj_1.valve.name);
-                next_1.weight += curr.v1.weight;
-                for (const auto& adj_2 : graph.at(curr.v2.valve)) {
-                    if (adj_1.valve.name == adj_2.valve.name || curr.v2.time <= adj_2.time || curr.isActivated(adj_2.valve.name)) {
+            for (const auto& adj_1 : filtered_adjs_1) {
+                for (const auto& adj_2 : filtered_adjs_2) {
+                    if (adj_1.valve.name == adj_2.valve.name) {
                         continue;
                     }
+                    Vertex next_1 {adj_1.valve, curr.v1.time - adj_1.time - 1};
+                    next_1.weight += curr.v1.weight;
+
                     Vertex next_2 {adj_2.valve, curr.v2.time - adj_2.time - 1};
-                    next_2.activated = curr.v2.activated;
-                    next_2.activated.insert(adj_2.valve.name);
                     next_2.weight += curr.v2.weight;
-                    DVertex next {next_1, next_2};
-                    int w = next.weight();
+
+                    int w = next_1.weight + next_2.weight;
                     DTValve node {TValve{adj_1.valve, next_1.time}, TValve{adj_2.valve, next_2.time}};
                     if (w > weight[node]) {
+                        next_1.activated = curr.v1.activated;
+                        next_1.activated.insert(adj_1.valve.name);
+                        next_2.activated = curr.v2.activated;
+                        next_2.activated.insert(adj_2.valve.name);
+                        DVertex next {next_1, next_2};
                         weight[node] = w;
                         pq.push(next);
                     }
@@ -248,31 +258,41 @@ int double_dijkstra(const WGraph& graph) {
         }
     }
 
-    int max_weight = 0;
-    for (const auto& [n, w] : weight) {
-        max_weight = std::max(max_weight, w);
-    }
-    return max_weight;
+    return std::accumulate(weight.cbegin(), weight.cend(), 0, [](int acc, const auto& w) -> int {
+        return std::max(acc, w.second);
+    });
 }
 
-void bfs(const Graph& graph, WGraph& wgraph, const Valve& start) {
-    std::set<Valve> visited;
-    std::queue<TValve> todo;
-    todo.push(TValve{start, 0});
-    while (!todo.empty()) {
-        TValve u = todo.front();
-        todo.pop();
-        if (visited.count(u.valve)) {
-            continue;
+WGraph compactify(const Graph& graph, const Valve& start) {
+    WGraph wgraph;
+
+    const auto bfs = [&graph, &wgraph](const Valve& start) -> void {
+        std::set<Valve> visited;
+        std::queue<TValve> todo;
+        todo.push(TValve{start, 0});
+        while (!todo.empty()) {
+            TValve u = todo.front();
+            todo.pop();
+            if (visited.count(u.valve)) {
+                continue;
+            }
+            visited.insert(u.valve);
+            if (u.valve != start && u.valve.flow > 0) {
+                wgraph[start].push_back(u);
+            }
+            for (const auto& v : graph.at(u.valve)) {
+                todo.emplace(TValve{v, u.time + 1});
+            }
         }
-        visited.insert(u.valve);
-        if (!(u.valve == start) && u.valve.flow > 0) {
-            wgraph[start].push_back(u);
-        }
-        for (const auto& v : graph.at(u.valve)) {
-            todo.emplace(TValve{v, u.time + 1});
+    };
+
+    bfs(start);
+    for (const auto& [valve, connections] : graph) {
+        if (valve.flow > 0) {
+            bfs(valve);
         }
     }
+    return wgraph;
 }
 
 int main() {
@@ -289,16 +309,18 @@ int main() {
             line.erase(std::remove_if(line.begin(), line.end(), [](char tok) -> bool {
                 return !std::isspace(tok) && !std::isupper(tok) && !std::isdigit(tok);
             }), line.end());
+            line.erase(line.begin());  // Remove V from Valve
             std::istringstream iss {line};
             std::string name, connected;
             int flow;
-            iss >> name >> name >> flow;
+            iss >> name >> flow;
             flow_map[name] = flow;
             while (iss >> connected) {
                 adj[name].push_back(connected);
             }
         }
 
+        Valve start {"AA", flow_map.at("AA")};
         Graph graph;
         for (const auto& [name_a, connected] : adj) {
             Valve a {name_a, flow_map.at(name_a)};
@@ -308,23 +330,10 @@ int main() {
             }
         }
 
-        WGraph wgraph;
-        bfs(graph, wgraph, Valve{"AA", 0});
-        for (const auto& [u, vs] : graph) {
-            if (u.flow > 0) {
-                bfs(graph, wgraph, u);
-            }
-        }
-        // for (const auto& [u, vs] : wgraph) {
-        //     std::cout << u << " has adjacent nodes:\n";
-        //     for (const auto& v : vs) {
-        //         std::cout << v.valve << " at time " << v.time << ' ';
-        //     }
-        //     std::cout << '\n';
-        // }
+        WGraph wgraph = compactify(graph, start);
 
-        int ans_one = dijkstra(wgraph);
-        int ans_two = double_dijkstra(wgraph);
+        int ans_one = single_dijkstra(wgraph, start, 30);
+        int ans_two = double_dijkstra(wgraph, start, 26);
 
         std::cout << file << ":\n";
         std::cout << "Answer part 1:  " << ans_one << '\n';
@@ -333,59 +342,11 @@ int main() {
     return 0;
 }
 
-
-
-
-// int dijkstra2(const Graph& graph) {
-
-//     std::map<TValve, int> weight;
-//     // for (const auto& pair : graph) {
-//     //     weight[pair.first.name] = 0;
-//     // }
-//     std::string start = "AA";
-//     std::priority_queue<Vertex> pq;
-//     pq.push(Vertex(start, 0, 30));
-//     //std::set<Vertex> processed;
-//     //weight[Node{start, 30}].value = 0;
-//     while (!pq.empty()) {
-//         Vertex curr = pq.top();
-//         std::cout << "Pop vertex: " << curr << '\n';
-//         pq.pop();
-//         if (curr.time <= 2) {
-//             continue;
-//         }
-//         // if (processed.count(curr)) {
-//         //     continue;
-//         // }
-//         // processed.insert(curr);
-//         for (const auto& adj : graph.at(curr.valve)) {
-//             Vertex next {adj, curr.time - 1};
-//             int w = curr.weight;
-//             TValve node {adj, next.time};
-//             if (w > weight[node]) {
-//                 next.activated = curr.activated;
-//                 next.weight = w;
-//                 weight[node] = w;
-//                 pq.push(next);
-//             }
-//             if (adj.flow > 0 && std::find(curr.activated.begin(), curr.activated.end(), adj.name) == curr.activated.end()) { //!curr.activated.count(adj.name)) {
-//                 Vertex next {adj, curr.time - 2};
-//                 int w = next.weight + curr.weight;
-//                 TValve node {adj, next.time};
-//                 if (w > weight[node]) {
-//                     next.activated = curr.activated;
-//                     next.activated.push_back(adj.name);
-//                     next.weight = w;
-//                     weight[node] = w;
-//                     pq.push(next);
-//                 }
-//             }
-//         }
-//     }
-//     int max_weight = 0;
-//     for (const auto& [n, w] : weight) {
-//         //std::cout << n.name << " weight: " << w.value << '\n'; 
-//         max_weight = std::max(max_weight, w);
-//     }
-//     return max_weight;
-// }
+/*
+ * sample.txt:
+ * Answer part 1:  1651
+ * Answer part 2:  1707
+ * input.txt:
+ * Answer part 1:  1850
+ * Answer part 2:  2306
+ */
