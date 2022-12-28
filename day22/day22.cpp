1a4bef23c044f9f8f49338767db2c45ca9a4ebe5
Marcus Lassila <marcus@ducce.net>
2022-12-28 21:08:09 +0100

    day22 edit

--- day22/day22.cpp
+++ day22/day22.cpp
@@ -3,7 +3,6 @@
 #include <cmath>
 #include <cstddef>
 #include <fstream>
-#include <functional>
 #include <iostream>
 #include <map>
 #include <set>
@@ -52,15 +51,6 @@ Point operator+(const Point& p, const Point& q) {
 Point operator-(const Point& p, const Point& q) {
     return Point(p.x - q.x, p.y - q.y);
 }
-
-template<>
-struct std::hash<Point> {
-    std::size_t operator()(Point const& p) const noexcept {
-        std::size_t h1 = std::hash<int >{}(p.x);
-        std::size_t h2 = std::hash<int >{}(p.y);
-        return h1 ^ (h2 << 1);
-    }
-};
 
 template<std::size_t I>
 std::tuple_element_t<I, Point>& get(const Point& p) {
