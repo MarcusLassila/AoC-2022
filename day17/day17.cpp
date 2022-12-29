01c451076e9bafa89fe046ca3da7826085e9ada7
Marcus Lassila <marcus@ducce.net>
2022-12-29 14:12:35 +0100

    day17 refactor

--- day17/day17.cpp
+++ day17/day17.cpp
@@ -1,14 +1,18 @@
-#include <bits/stdc++.h>
-
-#define Int long long
+#include <algorithm>
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <string>
+#include <string_view>
+#include <utility>
 
 struct Point {
-    Int x, y;
+    int x, y;
     Point() : x(0), y(0) {}
-    Point(Int x, Int y) : x(x), y(y) {}
+    Point(int x, int y) : x(x), y(y) {}
 
     Point& operator+=(const Point& rhs);
-
     friend std::ostream& operator<<(std::ostream& os, const Point& p);
 };
 
@@ -43,32 +47,23 @@ Point operator-(const Point& p, const Point& q) {
     return Point(p.x - q.x, p.y - q.y);
 }
 
-template<>
-struct std::hash<Point> {  // https://en.cppreference.com/w/cpp/utility/hash
-    std::size_t operator()(Point const& p) const noexcept {
-        std::size_t h1 = std::hash<Int >{}(p.x);
-        std::size_t h2 = std::hash<Int >{}(p.y);
-        return h1 ^ (h2 << 1);
-    }
-};
-
 struct Rock {
     std::set<Point> points;
     Rock() {}
     Rock(const std::set<Point>& points) : points(points) {}
 
-    Rock move_up(Int steps = 1) const;
-    Rock move_down(Int steps = 1) const;
-    Rock move_left(Int steps = 1) const;
-    Rock move_right(Int steps = 1) const;
+    Rock move_up(int steps = 1) const;
+    Rock move_down(int steps = 1) const;
+    Rock move_left(int steps = 1) const;
+    Rock move_right(int steps = 1) const;
 
-    Int top() const;
-    Int bottom() const;
-    Int left_side() const;
-    Int right_side() const;
+    int top() const;
+    int bottom() const;
+    int left_side() const;
+    int right_side() const;
 };
 
-Rock Rock::move_up(Int steps) const {
+Rock Rock::move_up(int steps) const {
     Rock moved_rock;
     for (const auto& p : points) {
         moved_rock.points.insert({p.x - steps, p.y});
@@ -76,7 +71,7 @@ Rock Rock::move_up(Int steps) const {
     return moved_rock;
 }
 
-Rock Rock::move_down(Int steps) const {
+Rock Rock::move_down(int steps) const {
     Rock moved_rock;
     for (const auto& p : points) {
         moved_rock.points.insert({p.x + steps, p.y});
@@ -84,7 +79,7 @@ Rock Rock::move_down(Int steps) const {
     return moved_rock;
 }
 
-Rock Rock::move_left(Int steps) const {
+Rock Rock::move_left(int steps) const {
     Rock moved_rock;
     for (const auto& p : points) {
         moved_rock.points.insert({p.x, p.y - steps});
@@ -92,7 +87,7 @@ Rock Rock::move_left(Int steps) const {
     return moved_rock;
 }
 
-Rock Rock::move_right(Int steps) const {
+Rock Rock::move_right(int steps) const {
     Rock moved_rock;
     for (const auto& p : points) {
         moved_rock.points.insert({p.x, p.y + steps});
@@ -100,51 +95,28 @@ Rock Rock::move_right(Int steps) const {
     return moved_rock;
 }
 
-Int Rock::top() const {
+int Rock::top() const {
     return std::min_element(points.begin(), points.end(), [](const auto& p, const auto& q) -> bool {
         return p.x < q.x;
     })->x;
 }
 
-Int Rock::bottom() const {
+int Rock::bottom() const {
     return std::max_element(points.begin(), points.end(), [](const auto& p, const auto& q) -> bool {
         return p.x < q.x;
     })->x;
 }
 
-Int Rock::left_side() const {
+int Rock::left_side() const {
     return std::min_element(points.begin(), points.end(), [](const auto& p, const auto& q) -> bool {
         return p.y < q.y;
     })->y;
 }
 
-Int Rock::right_side() const {
+int Rock::right_side() const {
     return std::max_element(points.begin(), points.end(), [](const auto& p, const auto& q) -> bool {
         return p.y < q.y;
     })->y;
-}
-
-static const std::vector<Rock> rocks = {
-    std::set<Point>{{0, 2}, {0, 3}, {0, 4}, {0, 5}},
-    std::set<Point>{{0, 3}, {-1, 2}, {-1, 3}, {-1, 4}, {-2, 3}},
-    std::set<Point>{{0, 2}, {0, 3}, {0, 4}, {-1, 4}, {-2, 4}},
-    std::set<Point>{{0, 2}, {-1, 2}, {-2, 2}, {-3, 2}},
-    std::set<Point>{{0, 2}, {0, 3}, {-1, 2}, {-1, 3}},
-};
-
-void print(const std::set<Point>& occupied, Int high_point) {
-    for (Int x = high_point; x <= 0; ++x) {
-        std::string s = std::to_string(-x);
-        std::cout << s << std::string(3 - s.size(), ' ') << '|';
-        for (Int y = 0; y < 7; ++y) {
-            if (occupied.count({x, y})) {
-                std::cout << '#';
-            } else {
-                std::cout << '.';
-            }
-        }
-        std::cout << '\n';
-    }
 }
 
 bool intersects(const std::set<Point>& u, const std::set<Point>& v) {
@@ -156,143 +128,170 @@ bool intersects(const std::set<Point>& u, const std::set<Point>& v) {
     });
 }
 
-void iterate(std::string_view moves, Rock rock, unsigned int& move_idx, std::set<Point>& occupied, Int& high_point) {
-    rock = rock.move_up(3 - high_point);
+namespace {
+    constexpr int simulator_width = 7;
+}
+
+class Simulator {
+public:
+    Simulator(std::string_view moves) : m_moves(moves) { find_cycle(); }
+
+    void reset();
+    void drop(const Rock& rock);
+    void drop_rocks(unsigned int num_rocks);
+    void find_cycle();
+
+    int height() const { return -m_top; }
+    bool is_occupied(const Point& p) const { return m_occupied.count(p); }
+    std::pair<unsigned int, unsigned int> periodicity() const { return {m_cycle_start_idx, m_cycle_length}; }
+
+    friend std::ostream& operator<<(std::ostream&, const Simulator&);
+
+private:
+    std::string m_moves;
+    std::set<Point> m_occupied;
+    int m_top = 0;
+    unsigned int m_move_idx = 0;
+    unsigned int m_cycle_start_idx;
+    unsigned int m_cycle_length;
+    static const inline std::array<Rock, 5> rocks = {
+        std::set<Point>{{0, 2}, {0, 3}, {0, 4}, {0, 5}},
+        std::set<Point>{{0, 3}, {-1, 2}, {-1, 3}, {-1, 4}, {-2, 3}},
+        std::set<Point>{{0, 2}, {0, 3}, {0, 4}, {-1, 4}, {-2, 4}},
+        std::set<Point>{{0, 2}, {-1, 2}, {-2, 2}, {-3, 2}},
+        std::set<Point>{{0, 2}, {0, 3}, {-1, 2}, {-1, 3}},
+    };
+    std::array<int, 7> relative_levels() const;
+};
+
+void Simulator::reset() {
+    m_occupied.clear();
+    m_top = 0;
+    m_move_idx = 0;
+}
+
+void Simulator::drop_rocks(unsigned int num_rocks) {
+    for (unsigned int i = 0; i < num_rocks; ++i) {
+        drop(rocks[i % rocks.size()]);
+    }
+}
+
+void Simulator::drop(const Rock& rock_inp) {
+    Rock rock = rock_inp.move_up(3 - m_top);
     while (true) {
-        switch (moves[move_idx]) {
+        switch (m_moves[m_move_idx]) {
             case '<':
                 if (rock.left_side() > 0) {
                     rock = rock.move_left();
-                    if (intersects(rock.points, occupied)) {
+                    if (intersects(rock.points, m_occupied)) {
                         rock = rock.move_right();
                     }
                 }
                 break;
             case '>':
-                if (rock.right_side() < 6) {
+                if (rock.right_side() + 1 < simulator_width) {
                     rock = rock.move_right();
-                    if (intersects(rock.points, occupied)) {
+                    if (intersects(rock.points, m_occupied)) {
                         rock = rock.move_left();
                     }
                 }
                 break;
         }
-        ++move_idx;
-        if (move_idx == moves.size()) {
-            move_idx = 0;
+        ++m_move_idx;
+        if (m_move_idx == m_moves.size()) {
+            m_move_idx = 0;
         }
         rock = rock.move_down();
-        if (intersects(rock.points, occupied) || rock.bottom() == 1) {
-            rock = rock.move_up();
-            high_point = std::min(rock.top() - 1, high_point);
-            occupied.insert(rock.points.begin(), rock.points.end());
+        if (intersects(rock.points, m_occupied) || rock.bottom() == 1) {
+            rock  = rock.move_up();
+            m_top = std::min(rock.top() - 1, m_top);
+            m_occupied.insert(rock.points.begin(), rock.points.end());
             break;
         }
     }
 }
 
-Int run_iterations(unsigned int iterations, std::string_view moves, unsigned int& move_idx, std::set<Point>& occupied, Int high_point) {
-    for (unsigned Int i = 0; i < iterations; ++i) {
-        Rock rock = rocks[i % rocks.size()];
-        iterate(moves, rock, move_idx, occupied, high_point);
-        // if (i == 13 || i == 14 || i == 15 || i == 16) {
-        //     print(occupied, high_point);
-        //     std::cout << '\n';
-        // }
-    }
-    return high_point;
-}
-
-Int part_one(std::string_view moves) {
-    Int high_point = 0;
-    unsigned int move_idx = 0;
-    std::set<Point> occupied;
-    return -run_iterations(2022, moves, move_idx, occupied, high_point);
-}
-
-std::array<Int, 7> bottom_scan(const std::set<Point>& occupied, Int high_point) {
-    std::array<Int, 7> levels;
-    levels.fill(0);
-    for (const auto& p : occupied) {
-        levels[p.y] = std::min(levels[p.y], p.x);
-    }
-    for (int k = 0; k < 7; ++k) {
-        levels[k] = levels[k] - high_point;
-    }
-    //std::cout << std::endl;
-    return levels;
-}
-
-std::pair<unsigned int, unsigned int> find_cycle(std::string_view moves) {
-    Int high_point = 0;
-    std::set<Point> occupied;
-    std::set<std::array<Int, 9>> seen;
-    unsigned int move_idx = 0;
-    Point start;
-    unsigned int i0 = 0;
+void Simulator::find_cycle() {
+    std::set<std::array<int, simulator_width + 2>> seen;  // Rock index, move index and relative height levels
+    Point start_indices;
     unsigned int i = 0;
     while (true) {
-        Rock rock = rocks[i % rocks.size()];
-        iterate(moves, rock, move_idx, occupied, high_point);
-        std::array<Int, 9> key;
-        key[0] = static_cast<Int>(i % rocks.size());
-        key[1] = static_cast<Int>(move_idx);
-        std::array<Int, 7> levels = bottom_scan(occupied, high_point);
-        for (int k = 2; k < 9; ++k) {
-            key[k] = levels[k - 2];
+        drop(rocks[i % rocks.size()]);
+        std::array<int, 9> key;
+        key[0] = static_cast<int>(i % rocks.size());
+        key[1] = static_cast<int>(m_move_idx);
+        std::array<int, 7> levels = relative_levels();
+        for (int k = 0; k < simulator_width; ++k) {
+            key[k + 2] = levels[k];
         }
         if (key[0] == 0 && seen.count(key)) {
-            i0 = i;
-            start = Point(key[0], key[1]);
+            m_cycle_start_idx = i;
+            start_indices = Point(key[0], key[1]);
             break;
         }
         seen.insert(key);
         ++i;
     }
     ++i;
-    unsigned int len = 1;
+    m_cycle_length = 1;
     while (true) {
-        Rock rock = rocks[i % rocks.size()];
-        iterate(moves, rock, move_idx, occupied, high_point);
-        Point indices {static_cast<int>(i % rocks.size()), static_cast<int>(move_idx)};
-        if (indices == start) {
+        drop(rocks[i % rocks.size()]);
+        Point indices {static_cast<int>(i % rocks.size()),
+                       static_cast<int>(m_move_idx)};
+        if (indices == start_indices) {
             break;
         }
-        ++len;
+        ++m_cycle_length;
         ++i;
     }
-    return {i0 - len, len};
+    m_cycle_start_idx -= m_cycle_length;
+    reset();
 }
 
+/* Used for finding the cycle */
+std::array<int, 7> Simulator::relative_levels() const {
+    std::array<int, simulator_width> levels;
+    levels.fill(0);
+    for (const auto& p : m_occupied) {
+        levels[p.y] = std::min(levels[p.y], p.x);
+    }
+    for (auto& lvl :levels) {
+        lvl -= m_top;
+    }
+    return levels;
+}
 
+std::ostream& operator<<(std::ostream& os, const Simulator& sim) {
+    for (int x = sim.height(); x >= 0; --x) {
+        auto s = std::to_string(x);
+        os << s << std::string(3 - s.size(), ' ') << '|';
+        for (int y = 0; y < simulator_width; ++y) {
+            os << (sim.is_occupied({-x, y}) ? '#' : '.');
+        }
+        os << '\n';
+    }
+    return os;
+}
 
-Int part_two(std::string_view moves) {
-    Int N = 1000000000000;
-    // Int high_point = 0;
-    // std::set<Point> occupied;
-    // high_point = run_iterations(100, moves, occupied, high_point);
-    // //print(occupied, high_point);
-    // return N;
-    auto [start, len] = find_cycle(moves);
-    std::cout << start << ' ' << len << '\n';
-    //unsigned Int start = 17;
-    //unsigned Int len = 35;
-    Int high_point = 0;
-    unsigned int move_idx = 0;
-    std::set<Point> occupied;
-    Int mile_stone = run_iterations(start, moves, move_idx, occupied, high_point);
-    Int diff = high_point - mile_stone;
-    high_point = mile_stone;
-    mile_stone = run_iterations(len, moves, move_idx, occupied, high_point);
-    diff = high_point - mile_stone;
-    N -= start + len;
+unsigned long long compute_height(std::string_view moves, unsigned long long drops) {
+    Simulator sim {moves};
+    const auto& [start, len] = sim.periodicity();
+    if (drops <= start + len) {
+        sim.drop_rocks(static_cast<unsigned int>(drops));
+        return sim.height();
+    }
 
-    Int quot = N / len;
-    Int rem  = N % len;
-    high_point = mile_stone;
-    mile_stone = run_iterations(rem, moves, move_idx, occupied, high_point);
-    std::cout << rem << '\n';
-    return -mile_stone + diff * quot;
+    sim.drop_rocks(start);
+    int ref = sim.height();
+    sim.drop_rocks(len);
+    int diff = sim.height() - ref;
+
+    drops -= start + len;
+    unsigned long long quot = drops / len;
+    unsigned int rem = static_cast<unsigned int>(drops % len);
+
+    sim.drop_rocks(rem);
+    return sim.height() + quot * diff;
 }
 
 int main() {
@@ -305,102 +304,19 @@ int main() {
         std::string moves;
         std::getline(input, moves);
 
-        Int ans_one = part_one(moves);
-        Int ans_two = part_two(moves);
+        Simulator sim {moves};
+
+        sim.drop_rocks(10);
+        std::cout << "First 10 blocks dropped:\n";
+        std::cout << sim << '\n';
+        sim.reset();
+
+        unsigned long long ans_one = compute_height(moves, 2022);
+        unsigned long long ans_two = compute_height(moves, 1000000000000);
 
         std::cout << file << ":\n";
         std::cout << "Answer part 1:  " << ans_one << '\n';
-        std::cout << "Answer part 2:  " << ans_two << '\n';
+        std::cout << "Answer part 2:  " << ans_two << "\n\n";
     }
     return 0;
 }
-
-
-/*
-
-1514285714288
-1514285714288
-
-..#.###
-.#####.
-....#..
-....#..
-....#..
-....#..
-.##.#..
-.##.#..
-..###..
-...#...
-..###..
-...#...
-..####.
-.....##
-.....##
-......#
-......#
-...####
-..###..
-...#...
-#..####
-#...#..
-#...#..
-#...##.
-##..##.
-######.
-.###...
-..#....
-.####..
-....##.
-....##.
-....#..
-..#.#..
-..#.#..
-#####..
-..###..
-...#...
-..####.
-....#..
-....#..
-..####.
-....###
-.....#.
-.#####.
-.#..#..
-.#..#..
-.####.#
-.####.#
-###.###
-.#####.
-.###...
-.###...
-.#.#...
-.#.#.#.
-.######
-.#####.
-....#..
-....#..
-....#..
-....#..
-.##.#..
-.##.#..
-..###..
-....#..
-...###.
-#...#..
-#####..
-#.#....
-#.#....
-####...
-..#####
-...#.##
-..####.
-.##....
-.##...#
-..#...#
-..#.###
-..#..#.
-..#.###
-.#####.
-
-
-*/
