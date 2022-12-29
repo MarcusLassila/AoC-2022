3473f3396977911806b82dbbf1d6b3797eee206a
Marcus Lassila <marcus@ducce.net>
2022-12-30 00:05:41 +0100

    day16 refactor

--- day16/day16.cpp
+++ day16/day16.cpp
@@ -1,26 +1,27 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <map>
 #include <numeric>
 #include <queue>
 #include <set>
 #include <sstream>
 #include <string>
-//#include <unordered_map>  // Maybe remove?
-//#include <unordered_set>  // Maybe remove?
+#include <string_view>
+#include <tuple>
+#include <utility>
 #include <vector>
+
 
 struct Valve {
     int flow;
     std::string name;
     Valve() : Valve{"", 0} {}
-    Valve(const std::string& name, int flow) : flow(flow), name(name) {}
+    Valve(std::string_view name, int flow) : flow(flow), name(name) {}
 
-    friend std::ostream& operator<<(std::ostream& os, const Valve& valve);
+    friend std::ostream& operator<<(std::ostream&, const Valve&);
 };
-
-using Graph = std::map<Valve, std::vector<Valve>>;
 
 std::ostream& operator<<(std::ostream& os, const Valve& valve) {
     os << '(' << valve.name << ',' << valve.flow << ')';
@@ -39,34 +40,29 @@ bool operator<(const Valve& a, const Valve& b) {
     return a.flow < b.flow || (a.flow == b.flow && a.name < b.name);
 }
 
-template<>
-struct std::hash<Valve> {
-    std::size_t operator()(Valve const& valve) const noexcept {
-        std::size_t h1 = std::hash<int>{}(valve.flow);
-        std::size_t h2 = std::hash<std::string>{}(valve.name);
-        return h1 ^ (h2 << 1);
-    }
-};
-
-struct TValve {
+struct TimeValve {
     Valve valve;
     int time;
-    TValve(const Valve& valve, int time) : valve(valve), time(time) {}
-    TValve(const std::string& name, int flow, int time) : TValve{Valve{name, flow}, time} {}
+    TimeValve(const Valve& valve, int time) : valve(valve), time(time) {}
+    TimeValve(std::string_view name, int flow, int time) : TimeValve{Valve{name, flow}, time} {}
 };
 
-using WGraph = std::map<Valve, std::vector<TValve>>;
-
-bool operator==(const TValve& a, const TValve& b) {
+bool operator==(const TimeValve& a, const TimeValve& b) {
     return a.time == b.time && a.valve == b.valve;
 }
 
-bool operator!=(const TValve& a, const TValve& b) {
+bool operator!=(const TimeValve& a, const TimeValve& b) {
     return !(a == b);
 }
 
-bool operator<(const TValve& a, const TValve& b) {
+bool operator<(const TimeValve& a, const TimeValve& b) {
     return b.time < a.time || (a.time == b.time && a.valve < b.valve);
+}
+
+template<std::size_t I>
+std::tuple_element_t<I, TimeValve>& get(const TimeValve& time_valve) {
+    if constexpr (I == 0) return time_valve.valve;
+    if constexpr (I == 1) return time_valve.time;
 }
 
 struct Vertex {
@@ -83,65 +79,77 @@ struct Vertex {
     Vertex(const Valve& valve, int time)
         : Vertex{valve, time, {}}
         {}
-    Vertex(const std::string& name, int flow, int time)
+    Vertex(std::string_view name, int flow, int time)
         : Vertex{Valve{name, flow}, time, {}}
         {}
-    Vertex(const std::string& name, int flow, int time, const std::set<std::string>& activated)
+    Vertex(std::string_view name, int flow, int time, const std::set<std::string>& activated)
         : Vertex{Valve{name, flow}, time, activated}
         {}
 
-    friend std::ostream& operator<<(std::ostream& os, const Vertex& vertex);
+    friend std::ostream& operator<<(std::ostream&, const Vertex&);
 };
 
 std::ostream& operator<<(std::ostream& os, const Vertex& vertex) {
     os << vertex.valve << " at time " << vertex.time << " has weight " << vertex.weight;
-    os << "\nhas used up: ";
+    os << "\nhas activated: ";
     for (const auto& v : vertex.activated) {
         os << v << ' ';
     }
     return os;
 }
 
+bool operator==(const Vertex& a, const Vertex& b) {
+    return a.weight == b.weight && a.time == b.time && a.valve == b.valve;
+}
+
+bool operator!=(const Vertex& a, const Vertex& b) {
+    return !(a == b);
+}
+
 bool operator<(const Vertex& a, const Vertex& b) {
-    return a.weight < b.weight;  // If don't work, compare time and valves as well.
+    if (a.weight < b.weight) return true;
+    if (a.weight > b.weight) return false;
+    return a.time < b.time || (a.time == b.time && a.valve < b.valve);
 }
 
-struct DTValve {
-    TValve tv1;
-    TValve tv2;
-    DTValve(const TValve& a, const TValve& b) : tv1(a), tv2(b) {}
+struct VertexPair {
+    Vertex vertex_A;
+    Vertex vertex_B;
+    VertexPair(const Vertex& u, const Vertex& v) : vertex_A(u), vertex_B(v) {}
+
+    int weight() const { return vertex_A.weight + vertex_B.weight; }
+    bool is_activated(const std::string& name) const {
+        return vertex_A.activated.count(name) || vertex_B.activated.count(name);
+    }
+    friend std::ostream& operator<<(std::ostream&, const VertexPair&);
 };
 
-bool operator<(const DTValve& a, const DTValve& b) {
-    return a.tv1 < b.tv1 || (a.tv1 == b.tv1 && a.tv2 < b.tv2);
-}
-
-struct DVertex {
-    Vertex v1;
-    Vertex v2;
-    DVertex(const Vertex& u, const Vertex& v) : v1(u), v2(v) {}
-
-    int weight() const { return v1.weight + v2.weight; }
-    bool isActivated(const std::string& name) const { return v1.activated.count(name) || v2.activated.count(name); }
-
-    friend std::ostream& operator<<(std::ostream& os, const DVertex& dv);
-};
-
-std::ostream& operator<<(std::ostream& os, const DVertex& dv) {
-    os << "Human " << dv.v1 << '\n' << "Elephant " << dv.v2;
+std::ostream& operator<<(std::ostream& os, const VertexPair& dv) {
+    os << "Human " << dv.vertex_A << '\n' << "Elephant " << dv.vertex_B;
     return os;
 }
 
-bool operator<(const DVertex& a, const DVertex& b) {
-    return a.weight() < b.weight();
+bool operator==(const VertexPair& a, const VertexPair& b) {
+    return a.vertex_A == b.vertex_A && a.vertex_B == b.vertex_B;
 }
 
+bool operator!=(const VertexPair& a, const VertexPair& b) {
+    return !(a == b);
+}
 
-int single_dijkstra(const WGraph& graph, const Valve& start_at, int time_limit) {
-    std::map<TValve, int> weight;
-    Vertex start {start_at, time_limit};
+bool operator<(const VertexPair& a, const VertexPair& b) {
+    if (a.weight() < b.weight()) return true;
+    if (a.weight() > b.weight()) return false;
+    return a.vertex_A < b.vertex_A || (a.vertex_A == b.vertex_A && a.vertex_B < b.vertex_B);
+}
+
+using Graph         = std::map<Valve, std::vector<Valve>>;
+using WeightedGraph = std::map<Valve, std::vector<TimeValve>>;
+
+int single_dijkstra(const WeightedGraph& graph, const Valve& start, int time_limit) {
+    std::map<TimeValve, int> weight;
     std::priority_queue<Vertex> pq;
-    pq.push(start);
+    pq.emplace(start, time_limit);
     while (!pq.empty()) {
         Vertex curr = pq.top();
         pq.pop();
@@ -154,7 +162,7 @@ int single_dijkstra(const WGraph& graph, const Valve& start_at, int time_limit) 
             }
             Vertex next {adj.valve, curr.time - adj.time - 1};
             int w = next.weight + curr.weight;
-            TValve node {adj.valve, next.time};
+            TimeValve node {adj.valve, next.time};
             if (w > weight[node]) {
                 next.activated = curr.activated;
                 next.activated.insert(adj.valve.name);
@@ -170,88 +178,85 @@ int single_dijkstra(const WGraph& graph, const Valve& start_at, int time_limit) 
     });
 }
 
-int double_dijkstra(const WGraph& graph, const Valve& start_at, int time_limit) {
-    std::map<DTValve, int> weight;
+int double_dijkstra(const WeightedGraph& graph, const Valve& start_at, int time_limit) {
+    std::map<std::pair<TimeValve, TimeValve>, int> weight;
     Vertex start {start_at, time_limit};
-    std::priority_queue<DVertex> pq;
-    pq.emplace(DVertex{start, start});
+    std::priority_queue<VertexPair> pq;
+    pq.emplace(start, start);
 
-    // const auto double_activation = [](const DVertex& dv) -> bool {
-    //     for (const auto& n : dv.v1.activated) {
-    //         if (dv.v2.activated.count(n)) {
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // };
+    const auto relevant_adjacent_valves = [&graph](const auto& vertex, const auto& vertex_pair)
+    -> std::vector<TimeValve>
+    {
+        std::vector<TimeValve> ret;
+        std::vector<TimeValve> adjs = graph.at(vertex.valve);
+        std::copy_if(adjs.begin(),
+                     adjs.end(),
+                     std::back_inserter(ret),
+                     [&vertex, &vertex_pair](const auto& adj) -> bool {
+                         return vertex.time > adj.time && !vertex_pair.is_activated(adj.valve.name);
+                     });
+        return ret;
+    };
 
     while (!pq.empty()) {
-        DVertex curr = pq.top();
+        VertexPair curr = pq.top();
         pq.pop();
 
-        std::vector<TValve> filtered_adjs_1;
-        std::vector<TValve> adjs_1 = graph.at(curr.v1.valve);
-        std::copy_if(adjs_1.begin(), adjs_1.end(), std::back_inserter(filtered_adjs_1), [&curr](const auto& adj) -> bool {
-            return curr.v1.time > adj.time && !curr.isActivated(adj.valve.name);
-        });
-        std::vector<TValve> filtered_adjs_2;
-        std::vector<TValve> adjs_2 = graph.at(curr.v2.valve);
-        std::copy_if(adjs_2.begin(), adjs_2.end(), std::back_inserter(filtered_adjs_2), [&curr](const auto& adj) -> bool {
-            return curr.v2.time > adj.time && !curr.isActivated(adj.valve.name);
-        });
-        bool can_move_1 = !filtered_adjs_1.empty();
-        bool can_move_2 = !filtered_adjs_2.empty();
+        std::vector<TimeValve> filtered_adjs_A = relevant_adjacent_valves(curr.vertex_A, curr);
+        std::vector<TimeValve> filtered_adjs_B = relevant_adjacent_valves(curr.vertex_B, curr);
 
-        if (!can_move_1 && !can_move_2) {
+        if (filtered_adjs_A.empty() && filtered_adjs_B.empty()) {
             continue;
-        } else if (!can_move_1) {
-            for (const auto& adj : filtered_adjs_2) {
-                Vertex next {adj.valve, curr.v2.time - adj.time - 1};
+        } else if (filtered_adjs_A.empty()) {
+            for (const auto& [valve, time] : filtered_adjs_B) {
+                Vertex next {valve, curr.vertex_B.time - time - 1};
                 int w = next.weight + curr.weight();
-                DTValve node {TValve{curr.v1.valve, curr.v1.time}, TValve{adj.valve, next.time}};
+                auto node = std::make_pair(TimeValve{curr.vertex_A.valve, curr.vertex_A.time},
+                                           TimeValve{valve, next.time});
                 if (w > weight[node]) {
-                    next.activated = curr.v2.activated;
-                    next.activated.insert(adj.valve.name);
-                    next.weight  = curr.v2.weight + next.weight;
+                    next.activated = curr.vertex_B.activated;
+                    next.activated.insert(valve.name);
+                    next.weight  = curr.vertex_B.weight + next.weight;
                     weight[node] = w;
-                    pq.push(DVertex{curr.v1, next});
+                    pq.emplace(curr.vertex_A, next);
                 }
             }
-        } else if (!can_move_2) {
-            for (const auto& adj : filtered_adjs_1) {
-                Vertex next {adj.valve, curr.v1.time - adj.time - 1};
+        } else if (filtered_adjs_B.empty()) {
+            for (const auto& [valve, time] : filtered_adjs_A) {
+                Vertex next {valve, curr.vertex_A.time - time - 1};
                 int w = next.weight + curr.weight();
-                DTValve node {TValve{adj.valve, next.time}, TValve{curr.v2.valve, curr.v2.time}};
+                auto node = std::make_pair(TimeValve{valve, next.time},
+                                           TimeValve{curr.vertex_B.valve, curr.vertex_B.time});
                 if (w > weight[node]) {
-                    next.activated = curr.v1.activated;
-                    next.activated.insert(adj.valve.name);
-                    next.weight  = curr.v1.weight + next.weight;
+                    next.activated = curr.vertex_A.activated;
+                    next.activated.insert(valve.name);
+                    next.weight  = curr.vertex_A.weight + next.weight;
                     weight[node] = w;
-                    pq.push(DVertex{next, curr.v2});
+                    pq.emplace(next, curr.vertex_B);
                 }
             }
         } else {
-            for (const auto& adj_1 : filtered_adjs_1) {
-                for (const auto& adj_2 : filtered_adjs_2) {
-                    if (adj_1.valve.name == adj_2.valve.name) {
+            for (const auto& [valve_A, time_A] : filtered_adjs_A) {
+                for (const auto& [valve_B, time_B] : filtered_adjs_B) {
+                    if (valve_A.name == valve_B.name) {
                         continue;
                     }
-                    Vertex next_1 {adj_1.valve, curr.v1.time - adj_1.time - 1};
-                    next_1.weight += curr.v1.weight;
+                    Vertex next_A {valve_A, curr.vertex_A.time - time_A - 1};
+                    next_A.weight += curr.vertex_A.weight;
 
-                    Vertex next_2 {adj_2.valve, curr.v2.time - adj_2.time - 1};
-                    next_2.weight += curr.v2.weight;
+                    Vertex next_B {valve_B, curr.vertex_B.time - time_B - 1};
+                    next_B.weight += curr.vertex_B.weight;
 
-                    int w = next_1.weight + next_2.weight;
-                    DTValve node {TValve{adj_1.valve, next_1.time}, TValve{adj_2.valve, next_2.time}};
+                    int w = next_A.weight + next_B.weight;
+                    auto node = std::make_pair(TimeValve{valve_A, next_A.time},
+                                               TimeValve{valve_B, next_B.time});
                     if (w > weight[node]) {
-                        next_1.activated = curr.v1.activated;
-                        next_1.activated.insert(adj_1.valve.name);
-                        next_2.activated = curr.v2.activated;
-                        next_2.activated.insert(adj_2.valve.name);
-                        DVertex next {next_1, next_2};
+                        next_A.activated = curr.vertex_A.activated;
+                        next_A.activated.insert(valve_A.name);
+                        next_B.activated = curr.vertex_B.activated;
+                        next_B.activated.insert(valve_B.name);
                         weight[node] = w;
-                        pq.push(next);
+                        pq.emplace(next_A, next_B);
                     }
                 }
             }
@@ -263,15 +268,16 @@ int double_dijkstra(const WGraph& graph, const Valve& start_at, int time_limit) 
     });
 }
 
-WGraph compactify(const Graph& graph, const Valve& start) {
-    WGraph wgraph;
+/* Connect valves with flow > 0 by edges with weights corresponding to travel time. */
+WeightedGraph compactify(const Graph& graph, const Valve& start) {
+    WeightedGraph wgraph;
 
     const auto bfs = [&graph, &wgraph](const Valve& start) -> void {
         std::set<Valve> visited;
-        std::queue<TValve> todo;
-        todo.push(TValve{start, 0});
+        std::queue<TimeValve> todo;
+        todo.emplace(start, 0);
         while (!todo.empty()) {
-            TValve u = todo.front();
+            TimeValve u = todo.front();
             todo.pop();
             if (visited.count(u.valve)) {
                 continue;
@@ -281,7 +287,7 @@ WGraph compactify(const Graph& graph, const Valve& start) {
                 wgraph[start].push_back(u);
             }
             for (const auto& v : graph.at(u.valve)) {
-                todo.emplace(TValve{v, u.time + 1});
+                todo.emplace(v, u.time + 1);
             }
         }
     };
@@ -309,7 +315,7 @@ int main() {
             line.erase(std::remove_if(line.begin(), line.end(), [](char tok) -> bool {
                 return !std::isspace(tok) && !std::isupper(tok) && !std::isdigit(tok);
             }), line.end());
-            line.erase(line.begin());  // Remove V from Valve
+            line.erase(line.begin());  // Remove V from Valve which was not erased
             std::istringstream iss {line};
             std::string name, connected;
             int flow;
@@ -325,12 +331,11 @@ int main() {
         for (const auto& [name_a, connected] : adj) {
             Valve a {name_a, flow_map.at(name_a)};
             for (const auto& name_b : connected) {
-                Valve b {name_b, flow_map.at(name_b)};
-                graph[a].emplace_back(b);
+                graph[a].emplace_back(name_b, flow_map.at(name_b));
             }
         }
 
-        WGraph wgraph = compactify(graph, start);
+        WeightedGraph wgraph = compactify(graph, start);
 
         int ans_one = single_dijkstra(wgraph, start, 30);
         int ans_two = double_dijkstra(wgraph, start, 26);
